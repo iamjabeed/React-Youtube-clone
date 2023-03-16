cebf0f763d3d67ad6701d194a1a8e4ed85a4bcec
iamjabeed <smartyjabeedbhai001@gmail.com>
2023-03-16 20:02:09 +0530

    setup vite, tailwind css and installed some packages

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css"
+const root = document.getElementById("root");
+const rootElement = createRoot(root);
+rootElement.render(<App />);
